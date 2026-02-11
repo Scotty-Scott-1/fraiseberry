75c9a3787e10653a7b33c2e4918fe71672e97e4e
Solo <zen.bliss5728@eagereverest.com>
2026-02-11 11:11:35 +0100

    added conversation list outline (#1)
    Co-authored-by: Scotty-Scott-1

---
+++ client/src/components/ConversationList/ConversationList.jsx
@@ -0,0 +1,9 @@
+const ConversationnList = () => {
+	return (
+    <div >
+		<p>Hi</p>
+	</div>
+  );
+};
+
+export default ConversationnList;
---
+++ client/src/pages/ConversationList.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Utils/Header/Header";
+import ConversationnList from "../components/ConversationList/ConversationList";
+
+const ConversationnListPage = () => {
+  return(
+    <div>
+      <Header />
+      <ConversationnList />
+    </div>
+  );
+}
+export default ConversationnListPage;

4cf7d3657480037788057ae36527bbee2cbafbce
Scotty-Scott-1 <swilliamfr@outlook.com>
2026-02-16 16:46:14 +0100

    quick fix, db config

--- backend/database/config.js
+++ backend/database/config.js
@@ -3,14 +3,15 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
 
 if (!DB_NAME) throw new Error("❌ [MariaDB]: Missing database name.");
 if (!DB_USER) throw new Error("❌ [MariaDB]: Missing database user.");
 if (!DB_PASSWORD) throw new Error("❌ [MariaDB]: Missing database user password.");
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: "localhost",
+  host: DB_HOST || "localhost",
   dialect: "mariadb",
   logging: false,
   pool: {
